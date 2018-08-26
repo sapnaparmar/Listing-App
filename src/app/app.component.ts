8693661081bb3f0ff41ff1866abb1e42e7558452
sapna <sapna.parmar05@gmail.com>
2018-08-26 18:27:06 +0530

    adding new code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { cribs } from './../data/cribs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Listing-App...';
+  cribs: Array<any> = cribs;
 }
